5ac2de5b4c7c0d698908cae231efc34c619e2ea4
François Pluchino <francois.pluchino@gmail.com>
2018-06-05 10:44:00 +0200

    Remove plugin data on destroy method

--- js/pluginify.js
+++ js/pluginify.js
@@ -17,26 +17,31 @@ import $ from 'jquery';
  * @param {function}    ClassName   The class name
  * @param {boolean}     [shorthand] Check if the shorthand of jquery plugin must be added
  * @param {String|null} dataApiAttr The DOM data attribute selector name to init the jquery plugin with Data API, NULL to disable
+ * @param {String}      removeName  The method name to remove the plugin data
  */
-export default function (pluginName, dataName, ClassName, shorthand = false, dataApiAttr = null) {
+export default function (pluginName, dataName, ClassName, shorthand = false, dataApiAttr = null, removeName = 'destroy') {
     let old = $.fn[pluginName];
 
     $.fn[pluginName] = function(options = {}, ...args) {
         return this.each((i, element) => {
             let $this = $(element),
-                data = $this.data(dataName);
-
-            if (!data && options === 'destroy') {
-                return;
-            }
+                data = $this.data(dataName),
+                resFunc = undefined;
 
             if (typeof options === 'object') {
                 if(!data) {
                     $this.data(dataName, new ClassName(element, options));
                 }
+            } else if (typeof options === 'string' && data) {
+                if (data[options]) {
+                    resFunc = data[options].apply(data, args);
+                }
 
-            } else if (data && data[options]) {
-                return data[options].apply(data, args);
+                if (options === removeName) {
+                    $this.removeData(dataName);
+                }
+
+                return resFunc;
             }
         });
     };
